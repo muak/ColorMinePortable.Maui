e0a7fbf619a54d8593da818655689524ca69610f
Satoshi Nakamura <kamuxxx@hotmail.com>
2023-05-28 14:16:26 +0900

    First init

---
+++ ColorMinePortable.Maui.Tests/ConversionMauiTests.cs
@@ -0,0 +1,45 @@
+﻿using ColorMine.ColorSpaces;
+using NUnit.Framework;
+
+namespace ColorMine.Tests.ColorSpaces;
+
+[TestFixture]
+public class MauiTest
+{
+	private double diff = 0.01;
+		
+	[Test]
+	public void WhiteMauiToRgb()
+	{
+		var knownColor = Colors.White;
+		var expectedColor = new Rgb { R = 255, G = 255, B = 255, };
+
+		var conv = knownColor.To<Rgb>();
+		(conv.R - expectedColor.R < diff).IsTrue();
+		(conv.G - expectedColor.G < diff).IsTrue();
+		(conv.B - expectedColor.B < diff).IsTrue();
+	}
+	[Test]
+	public void GoldenrodMauiToRgb()
+	{
+		var knownColor = Color.FromRgb(218, 165, 32);
+		var expectedColor = new Rgb { R = 218, G = 165, B = 32, };
+
+		var conv = knownColor.To<Rgb>();
+		(conv.R - expectedColor.R < diff).IsTrue();
+		(conv.G - expectedColor.G < diff).IsTrue();
+		(conv.B - expectedColor.B < diff).IsTrue();
+	}
+	[Test]
+	public void MaroonMauiToRgb()
+	{
+		var knownColor = Colors.Maroon;
+		var expectedColor = new Rgb { R = 128, G = 0, B = 0 };
+
+		var conv = knownColor.To<Rgb>();
+		(conv.R - expectedColor.R < diff).IsTrue();
+		(conv.G - expectedColor.G < diff).IsTrue();
+		(conv.B - expectedColor.B < diff).IsTrue();
+	}
+
+}
---
+++ ColorMinePortable.Maui/MauiColorExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Maui.Graphics;
+
+namespace ColorMine.ColorSpaces;
+
+public static class MauiColorExtension
+{
+    /// <summary>
+    /// Convert Xamarin.Forms.Color to any IColorSpace
+    /// </summary>
+    /// <param name="color"></param>
+    /// <typeparam name="T">IColorSpace</typeparam>
+    public static T To<T>(this Color color) where T : IColorSpace, new()
+    {
+        var rgb = new Rgb {
+            R = color.Red * 255,
+            G = color.Green * 255,
+            B = color.Blue * 255,
+        };
+        return rgb.To<T>();
+    }
+
+    /// <summary>
+    /// Convert any IColorSpace to Xamarin.Forms.Color
+    /// </summary>
+    /// <returns>Xamarin.Forms.Color</returns>
+    /// <param name="color"></param>
+    public static Color ToMaui(this IColorSpace color)
+    {
+        var rgb = color.ToRgb();
+        return Color.FromRgb(
+            (int)Math.Round(rgb.R, 0),
+            (int)Math.Round(rgb.G, 0),
+            (int)Math.Round(rgb.B, 0)
+        );
+    }
+}
+
